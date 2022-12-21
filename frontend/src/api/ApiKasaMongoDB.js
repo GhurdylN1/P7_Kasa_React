40cac6c3c6809c3a84b44698f9d103977c6d4c4c
Ghurdyl <ghurdyl@gmail.com>
2022-12-21 16:51:18 +0100

    test deploy avec render, changement port backend

--- frontend/src/api/ApiKasaMongoDB.js
+++ frontend/src/api/ApiKasaMongoDB.js
@@ -1,7 +1,7 @@
 // axios pour toutes les requetes API entre le front et back
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'http://localhost:10000'
 
 export default axios.create({
   baseURL: BASE_URL,
