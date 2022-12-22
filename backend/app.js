aadd2114d0049b6c0529bc7d43c2083a364ccbcd
Ghurdyl <ghurdyl@gmail.com>
2022-12-22 08:57:36 +0100

    test deploy render

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 // app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://marasco-antony-kasa.onrender.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://marasco-antony-kasa.onrender.com');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
--- frontend/src/api/ApiKasaMongoDB.js
+++ frontend/src/api/ApiKasaMongoDB.js
@@ -1,7 +1,7 @@
 // axios pour toutes les requetes API entre le front et back
 import axios from 'axios'
 
-const BASE_URL = 'https://marasco-antony-kasa-api.onrender.com/'
+const BASE_URL = 'https://marasco-antony-kasa-api.onrender.com'
 
 export default axios.create({
   baseURL: BASE_URL,
