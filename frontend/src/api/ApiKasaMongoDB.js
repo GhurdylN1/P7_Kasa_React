cf3dcdc7965f3deb7cfd295a63855c06cc0a1646
Ghurdyl <ghurdyl@gmail.com>
2022-12-22 10:17:38 +0100

    update des urls pour le deploy Render

--- frontend/src/api/ApiKasaMongoDB.js
+++ frontend/src/api/ApiKasaMongoDB.js
@@ -1,8 +1,8 @@
 // axios pour toutes les requetes API entre le front et back
 import axios from 'axios'
 
-// const BASE_URL = 'https://marasco-antony-kasa-api.onrender.com'
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'https://marasco-antony-kasa-api.onrender.com'
+// const BASE_URL = 'http://localhost:5000'
 
 export default axios.create({
   baseURL: BASE_URL,
